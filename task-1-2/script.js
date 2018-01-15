871782b22da2b4ed597a2d030efbddc75f4086c7
MeLJes <meljesx@gmail.com>
2018-01-15 12:34:49 +0200

    Update filterByYears

--- task-1-2/script.js
+++ task-1-2/script.js
@@ -1,23 +1,40 @@
 // Sort by year
 function sortByYear() {
-    //var byDate = filmsInJSON.slice(0);
+    var filmsArr = filmsInJSON.slice(0);
 
-    filmsInJSON.sort(function(a, b) {
+    filmsArr.sort(function(a, b) {
         return a.year - b.year;
     });
-    //console.log('Sort by year:', filmsInJSON);
+    console.log('Sort by year:', filmsArr);
 }
-sortByYear();
+//sortByYear();
 
 
-// Filter by year
-function filterByYear(array, minYear, maxYear) {
+// Filter by years
+function filterByYears(array, minYear, maxYear) {
+    var filmsArr = filmsInJSON.slice(0);
 
+    // Undefined properties
+    minYear = minYear || undefined;
+    maxYear = maxYear || undefined;
+
+    if( array.length && minYear === undefined && maxYear === undefined ) {
+        console.log('Full films list:', filmsArr);
+    } else if ( array.length && minYear && minYear !== undefined && minYear !== null && minYear !== 'null' && maxYear === undefined ) {
+        var filmsWithMinYear =  filmsArr.filter(function (entry) {
+            return entry.year >= minYear;
+        });
+        console.log('In the range from min year:', filmsWithMinYear);
+    } else if ( minYear === null || minYear === 'null' ) {
+        var filmsWithMaxYearOnly = filmsInJSON.filter(function (entry) {
+            return entry.year <= maxYear;
+        });
+        console.log('In the range up to the maximum date:', filmsWithMaxYearOnly);
+    } else if ( array.length && maxYear && maxYear !== undefined ) {
+        var filmsWithMaxYear = filmsInJSON.filter(function (entry) {
+            return entry.year >= minYear && entry.year <= maxYear;
+        });
+        console.log('In the range from the min to the max date:', filmsWithMaxYear);
+    }
 }
-//filterByYear(array, null, 2000);
-
-
-var testForTest = filmsInJSON.filter(function (entry) {
-    return entry.year >= 2000 && entry.year <= 2010;
-});
-console.log(testForTest);
+//filterByYears('array', 'null', 2000);
