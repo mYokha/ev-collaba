cb932f62a7aaf81997ae3d5ff2340687f909767e
MeLJes <meljesx@gmail.com>
2018-01-17 17:10:16 +0200

    getAmountByGenres() done

--- task-1-2/script.js
+++ task-1-2/script.js
@@ -15,7 +15,7 @@ function filterByYears(array, minYear, maxYear) {
     var filmsArr = filmsInJSON.slice(0);
 
     // Undefined properties
-    minYear = minYear || undefined;
+    minYear = minYear || undefined || null;
     maxYear = maxYear || undefined;
 
     if( array.length && minYear === undefined && maxYear === undefined ) {
@@ -26,15 +26,49 @@ function filterByYears(array, minYear, maxYear) {
         });
         console.log('In the range from min year:', filmsWithMinYear);
     } else if ( minYear === null || minYear === 'null' ) {
-        var filmsWithMaxYearOnly = filmsInJSON.filter(function (entry) {
+        var filmsWithMaxYearOnly = filmsArr.filter(function (entry) {
             return entry.year <= maxYear;
         });
         console.log('In the range up to the maximum date:', filmsWithMaxYearOnly);
     } else if ( array.length && maxYear && maxYear !== undefined ) {
-        var filmsWithMaxYear = filmsInJSON.filter(function (entry) {
+        var filmsWithMaxYear = filmsArr.filter(function (entry) {
             return entry.year >= minYear && entry.year <= maxYear;
         });
         console.log('In the range from the min to the max date:', filmsWithMaxYear);
     }
 }
 //filterByYears('array', 'null', 2000);
+
+
+// Get amount by genres
+function getAmountByGenres() {
+    var filmsArr = filmsInJSON.slice(0);
+    var holder = [];
+    var genreArr = {};
+
+    // Grub all genres
+    var getAllGenres = filmsArr.forEach(function (entry) {
+        holder.push(entry.genre);
+    });
+
+    // Get unique genres
+    var genreHolder = holder.filter(function(item, i, ar) {
+        // I have no idea about arguments and why they goes in this order
+        return ar.indexOf(item) === i;
+    });
+
+    // Set new array of genres
+    for (var i = 0; i < genreHolder.length; i++) {
+        genreArr[genreHolder[i]] = 0;
+    }
+
+    // Calculates films
+    for (var el = 0; el < holder.length; el++) {
+        genreArr[holder[el]]++;
+    }
+    // I get a little bit of help, case i don't know, that 'key' = key in arr...)
+    // I think that one of loop we can pull down...
+
+    console.log('Get sum of each genre', genreArr);
+}
+//getAmountByGenres();
