26d3a15ef0b6eed5e27973bb1770ab173616a87b
Max Yokha <max.yokha@gmail.com>
2018-01-20 16:05:59 +0200

    implement getAmountByGenres function

--- task-1-2/script.js
+++ task-1-2/script.js
@@ -1,25 +1,31 @@
 // Basic array methods
 
-// Create function *sortByYear* that accepts films array and returns array __sorted by year__
+// Create function *sortByYear* that accepts films array and returns array sorted by year
 const sortByYear = arr => arr.sort((a, b) => a.year - b.year)
 // console.log(sortByYear(filmsInJSON))
 
 // Create function *filterByYears* that has three parametrs (array, minYear, maxYear) but only
 // first one is required. If we only pass array it just returns it. If we pass second parameter
-// it filters it by __minimum release year__, if we pass min and max it filters __between these
-// two years__. Also we can pass (array, null, 2000) that should return __all films before 2000__.
+// it filters it by minimum release year, if we pass min and max it filters between these
+// two years. Also we can pass (array, null, 2000) that should return all films before 2000.
 
 const filterByYears = (array, minYear, maxYear) => {
-    if (minYear && !maxYear) {
-        return array.filter(el => el.year >= minYear)
-    } else if (!minYear && maxYear) {
-        return array.filter(el => el.year <= maxYear)
-    } else if (minYear && maxYear) {
-        return array.filter(el => el.year >= minYear && el.year <= maxYear)
-    }
+    if (minYear && !maxYear) return array.filter(el => el.year >= minYear)
+    if (!minYear && maxYear) return array.filter(el => el.year <= maxYear)
+    if (minYear && maxYear) return array.filter(el => el.year >= minYear && el.year <= maxYear)
     return array
 }
 
 // console.log(filterByYears(filmsInJSON))
 // console.log(filterByYears(filmsInJSON, 1995))
 // console.log(filterByYears(filmsInJSON, 1995, 1999))
+
+// Create function *getAmountByGenres* that calculates how many films of each genre are in the
+// array and returns an object like { drama: 5, comedy: 10, ... }
+
+const getAmountByGenres = arr => arr.reduce((genres, item) => {
+    genres[item.genre] ? genres[item.genre] ++ : genres[item.genre] = 1
+    return genres
+}, {})
+
+// console.log(getAmountByGenres(filmsInJSON))
